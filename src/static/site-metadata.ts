780a5ffb06a398b932c4327564959fd2fd2dea84
yinzhenbuzhike <yinzhenbuzhike233@163.com>
2026-05-14 15:45:33 +0800

    fix: update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -12,11 +12,11 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: '晚风踏星河的运动记录',
-  siteUrl: 'https://blog.4a1801.life',
-  logo: 'https://pan.4a1801.life:11443/d/public/logo1.jpg',
+  siteUrl: 'https://run.20260419.xyz',
+  logo: 'https://pub-91575f75878d40de9acc9395c2ce673a.r2.dev/%E9%87%91%E6%99%BA%E5%AA%9B.png',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
-  navLinks: [ ],// 清空导航链接
+  navLinks: [],
 };
 
 export default data;
